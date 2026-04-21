c39d180c1fd617f7692ff9ba5cb2116b73c77277
Malek <malek.azri@insat.ucar.tn>
2026-04-21 16:33:32 +0100

    is_sync = 0 | 1

--- create_databses.sql
+++
@@ -1,7 +0,0 @@
--- Création des bases de données
-CREATE DATABASE db_ho;
-CREATE DATABASE db_bo1;
-CREATE DATABASE db_bo2;
-
--- Vérification
-SHOW DATABASES;
--- insert_bo2.sql
+++
@@ -1,7 +0,0 @@
-USE db_bo2;
-
-INSERT INTO Product_Sales (Date, Region, Product, Qty, Cost, Amt, Tax, Total)
-VALUES
-('2024-04-01', 'West', 'Paper', 33, 12.95, 427.35, 29.91, 457.26),
-('2024-04-02', 'West', 'Pens', 40, 2.19, 87.60, 6.13, 93.73),
-('2024-04-03', 'West', 'Paper', 10, 12.95, 129.50, 9.07, 138.57);
